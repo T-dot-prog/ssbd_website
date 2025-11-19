7111a195efa812bfe75a08cddd886690162d7894
T-dot-prog <tahasinahoni2@gmail.com>
2025-11-19 22:07:37 +0600

    Added configuration

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def render_portal_info_card():
 
     # --- Initialize theme
     if "theme" not in st.session_state:
-        st.session_state.theme = "light"
+        st.session_state.theme = "dark"
 
     def toggle_theme():
         st.session_state.theme = "dark" if st.session_state.theme == "light" else "light"
