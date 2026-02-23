48ec9be0ff5ff3723abc48ec7e3599511c3e460f
T-dot-prog <tahasinahoni2@gmail.com>
2026-02-23 15:59:13 +0600

    Changes in data profile

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     description="A Streamlit web application for users in the SSBD Course",
     author="Tahasin Islam",
     author_email="tahasinahoni2@gmail.com",
-    packages=find_packages(),
+    packages= find_packages(),
     install_requires=["streamlit>=1.22", "redis", "openpyxl", "pydantic_settings", "python-pptx", "streamlit[pdf]", "pptxtopdf"],
     entry_points={"console_scripts": ["ssbd_website=ssbd_website.__main__:main"]},
     include_package_data=True,
